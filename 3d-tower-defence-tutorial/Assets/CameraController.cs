d3c7997a207ed061c07fb3970793aa780c837241
scottvossen <scottvossen@microsoft.com>
2021-10-15 09:39:47 -0500

    Support Pan and Zoom

---
+++ 3d-tower-defence-tutorial/Assets/CameraController.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private const string MouseScrollWheelAxis = "Mouse ScrollWheel";
+
+    private bool allowPan = true;
+
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
+    public float minX = -50f;
+    public float maxX = 100f;
+    public float minZ = -50f;
+    public float maxZ = 100f;
+
+    private void Update()
+    {
+        HandleTogglingMovement();
+
+        if (allowPan)
+        {
+            HandlePan();
+        }
+
+        HandleZoom();
+    }
+
+    private void HandleTogglingMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            allowPan = !allowPan;
+        }
+    }
+
+    private void HandlePan()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y > Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.mousePosition.y < 0 + panBorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x < 0 + panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x > Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        // place boundaries on how far we can pan
+        var position = transform.position;
+
+        position.x = Mathf.Clamp(transform.position.x, minX, maxX);
+        position.x = Mathf.Clamp(transform.position.z, minZ, maxZ);
+
+        transform.position = position;
+    }
+
+    private void HandleZoom()
+    {
+        var scroll = Input.GetAxis(MouseScrollWheelAxis);
+        var position = transform.position;
+
+        position.y -= scroll * scrollSpeed * Time.deltaTime * 1000;
+        position.y = Mathf.Clamp(position.y, minY, maxY);
+
+        transform.position = position;
+    }
+}
